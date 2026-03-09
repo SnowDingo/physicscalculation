bd4ddd8999b8d95769e848734f98243ed43c27c6
snowdingo <snowdingo.tech@gmail.com>
2026-03-09 18:16:41 +0900

    now the calculator can calculate the displacement using kinematics equations.

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ while(True):
         print("1: v0+at=vf\n 2: x=v0t+0.5at^2 \n 3: vf^2-vi^2=2ax")
         number = int(input("\n Please input the desired equation:"))
         if(number==1):
-            vi=int(input("put your vi: "))
+            condition = 0
+            vi=int(input("put your vi: "))   
             vf=int(input("put your vf: "))
             t=int(input("put your t: "))
             print(" The acceleration is: " +str((vf-vi)/t))
+            print("\n\n\n")
+        elif(number ==2):
+            condition = 0
+            vi=int(input("put your vi: "))   
+            t=int(input("put your t: "))
+            a=int(input("put your a: "))
+            print(" The displacement is: " +str((vi*t)+0.5*t*a^2))
             print("\n\n\n")
